b02bcf5b14a1c9cab51652250fa5ad48e518edd7
aerostride <aerostride@protonmail.com>
2020-01-29 10:16:29 +0100

    Update lookup.py

--- lookup.py
+++ lookup.py
@@ -1,6 +1,10 @@
 #snippet used only to print available lyrics from genius to user console
 
+#get API stuff
+
 import lyricsgenius
+
+#print list of songs from author
 
 genius = lyricsgenius.Genius("REDACTED")
 artist = genius.search_artist("CA$HANOVA BULHAR")
